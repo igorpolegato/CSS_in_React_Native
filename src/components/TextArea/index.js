d37a709495810c2d6c16806c01caec46d44a5588
Igor Polegato <igorpolegato@gmail.com>
2023-08-23 17:27:17 -0300

    Add text area

---
+++ src/components/TextArea/index.js
@@ -0,0 +1,14 @@
+import { View, Text } from "react-native";
+import { Container, ContainterText } from "./style";
+
+const TextArea = () => {
+    return (
+        <Container>
+            <ContainterText>
+                Styled components example
+            </ContainterText>
+        </Container>
+    )
+}
+
+export default TextArea;
---
+++ src/components/TextArea/style.js
@@ -0,0 +1,17 @@
+import styled from "styled-components/native";
+
+export const Container = styled.View`
+    width: 90%;
+    align-items: center;
+    background: #F2F2F2;
+    margin: auto;
+    border-radius: 10px;
+    padding: 10px;
+`
+
+export const ContainterText = styled.Text`
+    color: black;
+    font-size: 16px;
+    font-weight: 700;
+`
+
